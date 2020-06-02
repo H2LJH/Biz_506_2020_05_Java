70c1860276456737745cd9c88ccbaf61107db487
h2ljh <h2ljh@naver.com>
2020-06-02 13:15:34 +0900

    First

--- Java_Hello/src/Hello_02.java
+++ Java_Hello/src/Hello_02.java
@@ -7,7 +7,7 @@ public class Hello_02
 		System.out.println((30 - 40)  * 10);
 		
 		// 문자와 숫자를 연산할때  같은 연산자를 사용할때는 연산 우선순위를 명확하게 구분 해줘야 한다.
-		System.out.println("30 + 40 = " + 30 + 40); 
+		System.out.println("30 + 40 = " + 30 + 40);  
 		System.out.println("30 + 40 = " + (30 + 40));
 	}
 		
