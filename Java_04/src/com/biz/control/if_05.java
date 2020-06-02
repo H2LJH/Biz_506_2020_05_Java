9d8195a3f3c53fe59da06ef9008157c14c68a42e
h2ljh <h2ljh@naver.com>
2020-06-02 13:10:53 +0900

    If

--- Java_04/src/com/biz/control/if_05.java
+++ Java_04/src/com/biz/control/if_05.java
@@ -12,7 +12,7 @@ public class if_05
 		Random rnd = new Random();
 		int   num1 = rnd.nextInt(100)+1;
 		
-		if(num1 % 5 == 0) 
+		if(num1 % 5 == 0)  
 			System.out.printf("%d는 5의 배수\n", num1);
 		
 		else if(num1 % 4 == 0) 
