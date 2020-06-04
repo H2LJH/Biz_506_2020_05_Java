8b33efe443d148841d7c774aee4b3ade8925f1fc
ljh93 <ljh93@naver.com>
2020-06-04 19:18:05 +0900

    homeWork

--- Java_List/src/com/biz/list/service/ScoreServiceV2.java
+++ Java_List/src/com/biz/list/service/ScoreServiceV2.java
@@ -34,12 +34,13 @@ public class ScoreServiceV2
 	
 	public void scoreList()
 	{
+		int scoreSize = scoreList.size();
+		
 		System.out.println("=============================================");
 		System.out.println("\t\t성적 리스트");
 		System.out.println("=============================================");
 		System.out.println("학번\t국어\t영어\t수학\t총점\t평균");
 		System.out.println("=============================================");
-		int scoreSize = scoreList.size();
 		for(int i=0; i<scoreSize; i++)
 		{
 			ScoreVO sVO = scoreList.get(i);
